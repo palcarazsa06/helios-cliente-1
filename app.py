401d51509e4865c50de7e026a9cb96cb52a7906f
palcarazsa06 <palcarazsa06@gmail.com>
2026-03-09 17:32:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,29 @@ from dotenv import load_dotenv
 
 import helios_master 
 
+
+# Justo después de los imports
+def check_password():
+    def password_entered():
+        if st.session_state["password"] == "DoraemonElGatoCosmico": # <--- Tu contraseña
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("Introduce la clave de acceso a Helios OS", type="password", on_change=password_entered, key="password")
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input("Introduce la clave de acceso a Helios OS", type="password", on_change=password_entered, key="password")
+        st.error("😕 Contraseña incorrecta")
+        return False
+    else:
+        return True
+
+if not check_password():
+    st.stop()  # Si no hay contraseña, no carga el resto de la web
+
 load_dotenv()
 
 st.set_page_config(page_title="Helios OS", page_icon="☀️", layout="wide")
